bb8d76010407955e432d12369032a8fc073010ee
Justin Wozniak <woz@anl.gov>
2024-11-08 15:11:00 -0600

    Improve installer

--- alt-scores/install.sh
+++ alt-scores/install.sh
@@ -1,1 +1,12 @@
-pip install scikit-learn matplotlib pandas
+#!/bin/zsh
+set -eu
+
+which pip
+read -t 3 || true
+PKGS=(
+  scikit-learn matplotlib pandas
+  "tensorflow==2.15"
+)
+
+set -x
+pip install $PKGS
