d98593ed2d04985c7b3de499f7b326d637a20156
Justin Wozniak <woz@anl.gov>
2024-11-21 13:07:30 -0600

    Include size, date

--- cell-grep/stat-parquet.py
+++ cell-grep/stat-parquet.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python
 
+
+"""
+STAT PARQUET PY
+
+Pipe to column -t | sort -n -k 4
+"""
+
+import datetime
+import os
 import sys
 import pandas as pd
 
 filenames = sys.argv[1:]
 
 for filename in filenames:
+
+    print(filename, end=" ")
     try:
+        s = os.stat(filename)
+        print(format(s.st_size, ",.0f"), end=" ")
+        dt = datetime.datetime.fromtimestamp(s.st_mtime)
+        print(dt.strftime("%Y-%m-%d %H:%M:%S"), end=" ")
         df_rsp = pd.read_parquet(filename)
     except Exception as e:
-        print("show-parquet.py: ERROR")
+        print("show-parquet.py: ERROR for file: '%s'" % filename)
         print(str(e))
         exit(1)
 
-    prefix = filename + ":"
-    print("%-24s %5i" % (prefix, len(df_rsp)))
+    print("%i" % len(df_rsp))
