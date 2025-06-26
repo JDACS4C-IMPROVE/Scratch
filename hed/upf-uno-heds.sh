0443fb9552e35bd07fa816754984c920d3c744eb
Justin Wozniak <woz@anl.gov>
2025-06-26 14:08:26 -0500

    Support restarts, DATA_SOURCE

--- hed/upf-uno-heds.sh
+++ hed/upf-uno-heds.sh
@@ -3,15 +3,13 @@ set -eu
 
 # UPF UNO HED
 
-if (( ${#} != 3 ))
-then
-  echo "usage: upf-uno-hed.sh SITE UPF DFLTS"
-  exit 1
-fi
-
-SITE=$1
-export UPF=$2
-export UPF_DFLTS=$3
+case ${#} in
+  3) SITE=$1 UPF=$2 DFLTS=$3 RESTART="" ;;
+  4) SITE=$1 UPF=$2 DFLTS=$3 RESTART=$4 ;;
+  *) echo "usage: upf-uno-hed.sh SITE UPF DFLTS RESTART"
+     exit 1 ;;
+esac
+export UPF UPF_DFLTS=$DFLTS RESTART
 
 SUPERVISOR_TOOL=$( which supervisor || echo NONE )
 if [[ $SUPERVISOR_TOOL == NONE ]]
@@ -35,8 +33,12 @@ CFG_SYS=cfg-sys-1.sh
 
 export CANDLE_FRAMEWORK="keras"
 export CANDLE_MODEL_TYPE="BENCHMARKS"
-# export MODEL_NAME="uno_train_improve"
 export MODEL_NAME="unorun"
+# Inputs on permanent FS:
+# Small data:
+export DATA_SOURCE=/home/wozniak/proj/I-UNO.jw/exp_result
+# Big data:
+# export DATA_SOURCE=/lus/flare/projects/candle_aesp_CNDA/wozniak/raw_data_3-26-2025-preprocessed
 # Used for output:
 # Dunedin:
 # export CANDLE_DATA_DIR=/usb3/woz/CANDLE_DATA_DIR
@@ -53,11 +55,12 @@ export IMPROVE_LOG_LEVEL=WARN
 
 export TURBINE_LEADER_HOOK_STARTUP="$( sed 's/#.*//;s/$/;/' ${THIS}/hook-leader.tcl )"
 
-
 export TURBINE_PRELAUNCH="source $THIS/prelaunch.sh"
 
-export TURBINE_LOG=1 TURBINE_DEBUG=1 \
-       ADLB_DEBUG=1 ADLB_DEBUG_RANKS=1 ADLB_DEBUG_HOSTMAP=1
+# export TURBINE_LOG=1 TURBINE_DEBUG=1 \
+#        ADLB_DEBUG=1 ADLB_DEBUG_RANKS=1 ADLB_DEBUG_HOSTMAP=1
+
+export ADLB_DEBUG_RANKS=1 ADLB_DEBUG_HOSTMAP=1
 
 echo CANDLE_MODEL_NAME
 set -x
