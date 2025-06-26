00130e4b472abbe4426538c80a9fa7b6fc2288bc
Justin Wozniak <woz@anl.gov>
2025-06-26 14:08:47 -0500

    Export data to tar for xfer

---
+++ hed/export.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -eu
+
+# EXPORT SH
+# Create TGZ with key experiment inputs and outputs
+
+if ! SUPERVISOR_HOME=$( supervisor -H )
+then
+  log "Add supervisor to PATH!"
+  exit 1
+fi
+
+# For log():
+LOG_NAME="export.sh"
+source $SUPERVISOR_HOME/workflows/common/sh/utils.sh
+
+SIGNATURE -H "Provide an output DIR (e.g., .../X042/out)!" \
+          DIR - ${*}
+
+if ! [[ -d $DIR ]]
+then
+  abort "Does not exist: $DIR"
+fi
+
+OUT=$( dirname  $DIR )
+EXP=$( basename $DIR )
+cd $OUT
+
+log "DIR:" $DIR
+
+QUERY=( -name '*.json'        -or
+        -name parameters.txt  -or
+        -name '*predicted.csv' )
+
+RESULTS=( $( find $EXP ${QUERY[@]} | sort ) )
+
+FILES=( $EXP/jobid.txt
+        $EXP/hed-*.json
+        $EXP/dflts-*.json
+        $EXP/scores.csv
+        $EXP/turbine.log
+        $EXP/turbine-env.txt
+        $EXP/turbine-pbs.sh
+        ${RESULTS[@]} )
+
+TGZ=$DIR/$EXP.tgz
+log "packing ${#FILES[@]} inputs ..."
+tar cfz $DIR/$EXP.tgz ${FILES[@]}
+log "wrote:" $( basename $TGZ ) 
