eeade57bd25054d6fed68e723e52dcb364457ad8
Justin Wozniak <woz@anl.gov>
2025-07-03 13:42:06 -0500

    Clean up output

--- hed/prelaunch.sh
+++ hed/prelaunch.sh
@@ -11,18 +11,18 @@ set -eu
 
 echo "PRELAUNCH.SH ..."
 
+# Turbine installation w/o Python for use of mpi-cp:
 TURBINE_PLAIN_HOME=/lus/flare/projects/candle_aesp_CNDA/sfw/aurora/swift-t/2025-06-03-tcl
+MPI_CP=$TURBINE_PLAIN_HOME/turbine/bin/mpi-cp
 
 TAR=/lus/flare/projects/candle_aesp_CNDA/sfw/TF.tar
 P=( -n $NODES -ppn 1 )
-echo "MPI-CP ..."
-set -x
+echo "MPI-CP $TAR ..."
 /usr/bin/time --format="MPI-CP: %E" \
-  ${TURBINE_LAUNCHER} ${P[@]} $TURBINE_PLAIN_HOME/turbine/bin/mpi-cp $TAR /tmp
+  ${TURBINE_LAUNCHER} ${P[@]} $MPI_CP $TAR /tmp
 echo "UNTAR ..."
 /usr/bin/time --format="UNTAR: %E" \
   ${TURBINE_LAUNCHER} ${P[@]} tar -xm -f /tmp/TF.tar -C /tmp
-set +x
 
 set +eu
 module load frameworks
@@ -30,6 +30,7 @@ module load frameworks
 set -eu
 PATH=/tmp/TF/bin:$PATH
 
+# Force rank prefixes in main file output.txt:
 export TURBINE_LAUNCH_OPTIONS="-l"
 
 echo "PRELAUNCH.SH DONE."
